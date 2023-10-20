16a1a542ee1e74343921590d6bb827d82a58b403
ericgaona <eric@Geraldines-Air.station>
2023-10-20 23:58:55 +0100

    adding the main image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,16 @@
+import image from "../assets/images/img01.jpg";
+
 function Hero() {
+  const containerStyle = {
+    backgroundImage: `url('${image}')`,
+  };
+
   return (
     <div
       id="hero"
       className="h-[calc(100vh)] bg-red-200
-     flex flex-col justify-center
-     bg-[url('https://ericgaona.com/images/img01.jpg')]
-     bg-cover bg-center
-     "
+     flex flex-col justify-center bg-cover bg-center"
+      style={containerStyle}
     >
       <h1 className="text-white font-bold text-5xl">Eric Gaona</h1>
       <p className="text-white font-bold text-3xl">Full-Stack Web Developer</p>
