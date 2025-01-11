9d8b0654659fdcc9b551fff83ee615aecba7177c
eric <ericgaona0413@gmail.com>
2025-01-11 17:18:41 +0000

    update the About component

--- src/components/About.js
+++ src/components/About.js
@@ -20,14 +20,14 @@ const About = () => {
       <h2 className="font-bold text-2xl mb-3">About</h2>
       <div className="flex flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow p-10 gap-5">
         <div className="order-last md:order-first">
-          <p className="font-normal text-black text-left mb-5 text-2xl">
+          <p className="font-normal text-black text-left mb-5 text-1xl">
             Thanks for stopping by. My name is Eric. I'm a freelance Web
             Developer from Spain. I have been living in Dublin for a few years
             now, so I am happy to work with you in both Spanish and English. I
             can help you with the planning and creation of your new website or
             even improve your old one.
           </p>
-          <p className="font-normal text-black text-left text-2xl">
+          <p className="font-normal text-black text-left mb-5 text-1xl">
             I use a mixture of HTML, CSS, Bootstrap, Javascript, PHP, Python,
             Flask, Django, React, Next.js, MongoDB, MySQL and WordPress.{" "}
             <a
@@ -39,6 +39,9 @@ const About = () => {
             </a>{" "}
             today and we can start discussing options and ideas for your own
             digital platform.
+          </p>
+          <p className="font-normal text-black text-left text-1xl">
+          I created this portfolio website using HTML, CSS, Tailwind, JavaScript, React, Github, and FormSubmit as a third-party API. It is hosted on Blacknight.
           </p>
         </div>
         <img
